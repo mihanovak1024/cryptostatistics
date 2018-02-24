71f0dad0ba7487a3157ab043ab1460f45cb67b25
Miha Novak <miha.novak@outfit7.com>
2018-02-24 12:04:34 +0100

    initial commit

---
+++ app/src/main/java/oreschnix/cryptosatistics/Main.java
@@ -0,0 +1,17 @@
+package oreschnix.cryptosatistics;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by miha.novak on 23/02/2018.
+ */
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
